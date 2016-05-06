a22a60a614255afdb92b57bbd7d16d10da1038fa
Wesley Guirra <Wesley Guirra>
2016-05-06 07:22:14 -0300

    Adicionado Bootstrap, Flaticon e header

--- wp-content/themes/argento/header.php
+++ wp-content/themes/argento/header.php
@@ -16,6 +16,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="wp-content/themes/argento/css/bootstrap.min.css">
+<link rel="stylesheet" href="wp-content/themes/argento/css/argento.custom.css">
+<link rel="stylesheet" href="wp-content/themes/argento/css/flaticon.css">
 
 <?php wp_head(); ?>
 </head>
@@ -26,19 +29,44 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+		    <div class="container">
+		        <div class="row">
+                <?php
+                    if ( is_front_page() && is_home() ) : ?>
+                        <div class="site-logo col-md-2"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="wp-content/themes/argento/img/logo.svg" alt="Argento Desenvolvimento Profissional"></a></div>
+                    <?php else : ?>
+                        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <?php
+                    endif; ?>
+                    <div class="site-nav col-md-7 col-md-offset-3">
+                        <div class="col-md-8 col-md-offset-4">
+                            <ul class="pull-right">
+                                <li><i class="flaticon-busca"></i></li>
+                                <li><span><b>+55 11</b> 4329-7417</span></li>
+                                <li><span><b>+55 11</b> 4329-7447</span></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-12">
+                            <nav class="pull-right">
+                                <ul>
+                                    <li><a href=""><b>Nossos</b>Cursos</a></li>
+                                    <li><a href=""><b>Quem</b>Somos</a></li>
+                                    <li><a href=""><b>Fale</b>Conosco</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div><!-- .row -->
+			</div><!-- .container -->
+			<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 header-stripes">
+                        <div class="stripe-one"></div>
+                        <div class="stripe-two"></div>
+                        <div class="stripe-three"></div>
+                    </div>
+			    </div>
+			</div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
