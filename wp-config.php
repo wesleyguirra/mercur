156be31735d46952a48f2d784ff116390f10cfd0
Wesley Guirra <wesleyguirra@citr.co>
2016-05-26 10:52:40 -0300

    Configurado banco de dados Amazon RDS

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'argento');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'citrco');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '4?CudUQav$pA');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'argento.ci64apbc9tl9.us-west-2.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
