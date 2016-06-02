273915a04e52152b3a56984442a4a0d169fb551c
Wesley Guirra <wesleyguirra@citr.co>
2016-06-01 23:21:15 -0300

    Criar conteúdo página de cursos

--- wp-content/themes/argento/template-parts/content-cursos.php
+++ wp-content/themes/argento/template-parts/content-cursos.php
@@ -21,34 +21,34 @@
 					<h3>Ensino fundamental</h3> <span>[ ciclo i ]</span>
 					<div class="row">
 						<div class="col-md-6">
-							<div class="box-curso">
-								Leitura com autonomia
-							</div>
+							<a class="box-curso">
+								<span>Leitura com autonomia</span>
+							</a>
 						</div>
 						<div class="col-md-6">
-							<div class="box-curso">
-								Contexto de produção de texto
-							</div>
+							<a class="box-curso">
+								<span>Contexto de produção de texto</span>
+							</a>
 						</div>
 						<div class="col-md-6">
-							<div class="box-curso">
-								Alfabetização
-							</div>
+							<a class="box-curso">
+								<span>Alfabetização</span>
+							</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6">
-					<h3>Ensino fundamental</h3> <span>[ ciclo i]</span>
+					<h3>Ensino fundamental</h3> <span>[ ciclo ii e ensino médio ]</span>
 					<div class="row">
 						<div class="col-md-6">
-							<div class="box-curso">
-								Leitura com autonomia
-							</div>
+							<a class="box-curso">
+								<span>Desenvolvimento Profissional planejamento, gestão de sala de aula e avaliação</span>
+							</a>
 						</div>
 						<div class="col-md-6">
-							<div class="box-curso">
-								Contexto de produção de texto
-							</div>
+							<a class="box-curso">
+								<span>Desenvolvimento de habilidades das competências leitora e escritora</span>
+							</a>
 						</div>
 					</div>
 				</div>
