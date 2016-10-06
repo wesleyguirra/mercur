ed99ece3b06deb76c932baa93247786e6638f220
wesley <wesley@citr.co>
2016-10-06 18:02:51 -0300

    Dynamic user data in page quem-somos.php

--- wp-content/themes/argento/template-parts/content-quem-somos.php
+++ wp-content/themes/argento/template-parts/content-quem-somos.php
@@ -25,46 +25,33 @@
   </div>
 </div><!-- .container -->
 <div class="section-nossa-equipe">
+
+
+  <?php
+    $user_id = 2;
+    while ($user_id <= 5):
+      $user_info = get_userdata($user_id); ?>
+
+      <div class="nossa-equipe-member">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="nossa-equipe-member-info">
+                <h2 class="nossa-equipe-member-name"><?php echo $user_info->first_name ." ". $user_info->last_name; ?></h2>
+                <a href="<?php echo $user_info->user_url; ?>"><i class="fa fa-linkedin"></i><span>conecte-se</span></a>
+                <p class="nossa-equipe-member-about"><?php echo $user_info->description; ?></p>
+              </div><!-- .nossa-equipe-member-info -->
+              <div class="nossa-equipe-nav-arrows">
+                <i class="flaticon-left-arrow"></i>
+                <i class="flaticon-right-arrow"></i>
+              </div><!-- .nossa-equipe-nav-arrows -->
+            </div>
+            <div class="col-md-6">
+              <img src="http://argento.edu.s3.amazonaws.com/img/<?php echo $user_info->nickname; ?>.png" alt="<?php the_field('first_member_name'); ?>">
+            </div>
+          </div>
+        </div>
+      </div><!-- .nossa-equipe-member -->
+      <?php $user_id++;
+    endwhile; ?>
   <!-- Member -->
-  <div class="nossa-equipe-member milton">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="nossa-equipe-member-info">
-            <h2 class="nossa-equipe-member-name"><?php the_field('first_member_name'); ?></h2>
-            <a href=""><i class="fa fa-linkedin"></i><span>conecte-se</span></a>
-            <p class="nossa-equipe-member-about"><?php the_field('first_member_about'); ?></p>
-          </div><!-- .nossa-equipe-member-info -->
-          <div class="nossa-equipe-nav-arrows">
-            <i class="flaticon-left-arrow"></i>
-            <i class="flaticon-right-arrow"></i>
-          </div><!-- .nossa-equipe-nav-arrows -->
-        </div>
-        <div class="col-md-6">
-          <img src="http://argento.edu.s3.amazonaws.com/img/milton.png" alt="<?php the_field('first_member_name'); ?>">
-        </div>
-      </div>
-    </div>
-  </div><!-- .nossa-equipe-member -->
-  <!-- Member -->
-  <div class="nossa-equipe-member rosana">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="nossa-equipe-member-info">
-            <h2 class="nossa-equipe-member-name"><?php the_field('second_member_name'); ?></h2>
-            <a class="nossa-equipe-member-linkedin" href=""><i class="fa fa-linkedin"></i><span>conecte-se</span></a>
-            <p class="nossa-equipe-member-about"><?php the_field('second_member_about'); ?></p>
-          </div><!-- .nossa-equipe-member-info -->
-          <div class="nossa-equipe-nav-arrows">
-            <i class="flaticon-left-arrow"></i>
-            <i class="flaticon-right-arrow"></i>
-          </div><!-- .nossa-equipe-nav-arrows -->
-        </div>
-        <div class="col-md-6">
-          <img src="http://argento.edu.s3.amazonaws.com/img/rosana.png" alt="<?php the_field('second_member_name'); ?>">
-        </div>
-      </div>
-    </div>
-  </div><!-- .nossa-equipe-member -->
-</div><!-- .section-nossa-equipe -->
