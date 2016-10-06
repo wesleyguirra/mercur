5134cb34139b972bd1bc619d811c0ca914cbf1bc
wesley <wesley@citr.co>
2016-10-06 17:25:50 -0300

    Added User ID Column to functions.php

--- wp-content/themes/argento/functions.php
+++ wp-content/themes/argento/functions.php
@@ -315,3 +315,28 @@ function get_breadcrumbs() {
 		echo $wrap_after;
 	}
 } // end of dimox_breadcrumbs()
+
+// The Column User ID
+function rd_user_id_column( $columns ) {
+	$columns['user_id'] = 'ID';
+	return $columns;
+}
+add_filter('manage_users_columns', 'rd_user_id_column');
+
+/*
+ * Column content
+ */
+function rd_user_id_column_content($value, $column_name, $user_id) {
+	if ( 'user_id' == $column_name )
+		return $user_id;
+	return $value;
+}
+add_action('manage_users_custom_column',  'rd_user_id_column_content', 10, 3);
+
+/*
+ * Column style (you can skip this if you want)
+ */
+function rd_user_id_column_style(){
+	echo '<style>.column-user_id{width: 5%}</style>';
+}
+add_action('admin_head-users.php',  'rd_user_id_column_style');
